376a2678acdb48fafa3fc4f28e8eea32e351c5d0
wlaurance <w.laurance@gmail.com>
2014-12-15 15:46:34 -0500

    Put app into html 5 mode
    This gets rid of the hash bang

--- src/scripts/app/app.config.js
+++ src/scripts/app/app.config.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = /*@ngInject*/
-  function configure ($urlRouterProvider) {
+  function configure ($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/welcome/');
+
+    $locationProvider.html5Mode({
+      enabled: true
+    });
   };
