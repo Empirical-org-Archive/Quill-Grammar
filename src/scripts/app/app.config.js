239128710a7650b3d2332fe27876fdc0bc9e0e13
wlaurance <w.laurance@gmail.com>
2014-12-15 16:14:50 -0500

    Add index module

--- src/scripts/app/app.config.js
+++ src/scripts/app/app.config.js
@@ -2,7 +2,7 @@
 
 module.exports = /*@ngInject*/
   function configure ($urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise('/welcome/');
+    $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode({
       enabled: true
