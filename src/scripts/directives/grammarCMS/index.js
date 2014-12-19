6f70c28a0c9eb37275a9e6c7c68ccee59ed8e35d
wlaurance <w.laurance@gmail.com>
2014-12-19 14:19:25 -0500

    Use strict camel case for directive names
    Something was happening when angular normalized the directive name.

--- src/scripts/directives/grammarCMS/index.js
+++ src/scripts/directives/grammarCMS/index.js
@@ -4,6 +4,6 @@ module.exports =
 angular
   .module('quill-grammar.directives.grammarCMS', [
   ])
-  .directive('grammarCMSCategoriesNew', require('./categories.new.js'))
-  .directive('grammarCMSCategoriesList', require('./categories.list.js'))
-  .controller('GrammarCMSCtrl', require('./cms.controller.js'));
+  .directive('grammarCmsCategoriesNew', require('./categories.new.js'))
+  .directive('grammarCmsCategoriesList', require('./categories.list.js'))
+  .controller('GrammarCmsCtrl', require('./cms.controller.js'));
