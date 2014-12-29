2a87850ab1c58e668137df0f160e286a49a5bd97
wlaurance <w.laurance@gmail.com>
2014-12-29 12:57:28 -0500

    Add in some demo stub data

--- src/scripts/directives/grammarCMS/cms.controller.js
+++ src/scripts/directives/grammarCMS/cms.controller.js
@@ -7,7 +7,32 @@ function cmsController($scope) {
   /*
    * $scope initializers
    */
-  $scope.categories = [{title:'hey'}];
+  $scope.categories = [{title: '1.1b. Use Common, Proper, and Possessive Nouns.', rules: [
+    {
+      title: 'Proper Nouns {|11}',
+      questions: [
+        "Someday I will go to China",
+        "John Drops his son off at school",
+        "The paintings in Italy are Amazing"
+      ]
+    },
+    {
+      title: 'Common Nouns {|10}',
+      questions: [
+        "A cute duck is sewn onto the bag.",
+        "My father is a policeman",
+        "Did you see any sculptures in museum?"
+      ]
+    },
+    {
+      title: 'Possive Nouns {|12}',
+      questions: [
+        "Andy's favorite sport is basketball.",
+        "The church's towers are magnificent.",
+        "The ship's sail flaps in the wind.",
+      ]
+    }
+  ]}];
 
   $scope.saveCategory = function(category) {
 
