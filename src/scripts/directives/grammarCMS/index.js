73aab1a4a4a58ca1412a3e48e1de74abdf09a015
wlaurance <w.laurance@gmail.com>
2014-12-29 13:06:36 -0500

    Rule edit/Question WIP

--- src/scripts/directives/grammarCMS/index.js
+++ src/scripts/directives/grammarCMS/index.js
@@ -9,5 +9,8 @@ angular
   .directive('grammarCmsCategory', require('./category.show.js'))
   .directive('grammarCmsRule', require('./rule.show.js'))
   .directive('grammarCmsCategoryPanel', require('./category.edit.js').panel)
+  .directive('grammarCmsRulePanel', require('./rule.edit.js').panel)
+  .directive('grammarCmsRuleQuestion', require('./question.js').show)
+  .controller('RuleCtrl', require('./rule.edit.js').controller)
   .controller('CategoryCtrl', require('./category.edit.js').controller)
   .controller('GrammarCmsCtrl', require('./cms.controller.js'));
