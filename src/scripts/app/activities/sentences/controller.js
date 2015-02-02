bf5029cd300c12daf7cb82728e339b4a33105ea8
wlaurance <w.laurance@gmail.com>
2015-02-02 13:03:53 -0800

    Remove rule
    Do not add duplicates to the array.

--- src/scripts/app/activities/sentences/controller.js
+++ src/scripts/app/activities/sentences/controller.js
@@ -3,7 +3,7 @@
 module.exports =
 
 /*@ngInject*/
-function sentences($scope, CategoryService, $state, RuleService) {
+function sentences($scope, CategoryService, $state, RuleService, _) {
   $scope.newSentence = {};
   $scope.flags = [{$id:1, title: 'Production'}, {$id:2, title:'Beta'}];
 
@@ -23,7 +23,17 @@ function sentences($scope, CategoryService, $state, RuleService) {
     if (!$scope.newSentence.rules) {
       $scope.newSentence.rules = [];
     }
-    $scope.newSentence.rules.push(r);
+    if (_.find($scope.newSentence.rules, r)) {
+      throw new Error('Cannot have two instances of the same rule ' + r.title);
+    } else {
+      $scope.newSentence.rules.push(r);
+    }
+  };
+
+  $scope.removeRule = function(r) {
+    if ($scope.newSentence.rules) {
+      $scope.newSentence.rules = _.without($scope.newSentence.rules, r);
+    }
   };
 
 };
