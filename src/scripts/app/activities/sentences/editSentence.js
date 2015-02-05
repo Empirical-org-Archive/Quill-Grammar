941078d3c9ca9e6d60f3adf5015cc0b4c736a707
wlaurance <w.laurance@gmail.com>
2015-02-05 12:27:16 -0800

    Show rule details and the current category

--- src/scripts/app/activities/sentences/editSentence.js
+++ src/scripts/app/activities/sentences/editSentence.js
@@ -4,10 +4,18 @@ module.exports =
 
 /*@ngInject*/
 function EditSentence(
-  $scope, SentenceWritingService, $state
+  $scope, SentenceWritingService, $state, _
 ) {
   SentenceWritingService.getSentenceWriting($state.params.id)
     .then(function(s) {
+      s.category = _.findWhere($scope.availableCategories, function(o) {
+        return o.$id == s.categoryId;
+      });
+      s.rules = _.map(s.rules, function(r) {
+        var rr = _.findWhere($scope.availableRules, {$id: r.ruleId});
+        rr.quantity = r.quantity;
+        return rr;
+      });
       $scope.editSentence = s;
     });
 };
