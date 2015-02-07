6a3d18570eb530b48a05bfafe83a7c2e170c2c0c
wlaurance <w.laurance@gmail.com>
2015-02-06 17:27:53 -0800

    Select rule Question Samples.

--- src/scripts/app/play/sentences/controller.js
+++ src/scripts/app/play/sentences/controller.js
@@ -3,12 +3,21 @@ module.exports =
 
 /*@ngInject*/
 function SentencePlayCtrl(
-  $scope, $state, SentenceWritingService, RuleService
+  $scope, $state, SentenceWritingService, RuleService, _
 ) {
   $scope.id = $state.params.id;
 
   SentenceWritingService.getSentenceWriting($scope.id).then(function(sw) {
-    console.log(sw);
+    var ruleIds = _.pluck(sw.rules, 'ruleId');
+    var quantities = _.pluck(sw.rules, 'quantity');
+    RuleService.getRules(ruleIds).then(function(resolvedRules) {
+      $scope.swSet = _.chain(resolvedRules)
+        .map(function(rr, i) {
+          rr.selectedRuleQuestions = _.sample(rr.resolvedRuleQuestions, quantities[i]);
+          return rr;
+        })
+        .value();
+    });
   }, function() {
     $state.go('index');
   });
