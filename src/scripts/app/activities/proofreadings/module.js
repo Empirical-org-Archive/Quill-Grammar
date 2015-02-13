a2dd69c3f7d48b59136862145a38f2b2fef22af6
wlaurance <w.laurance@gmail.com>
2015-02-13 12:40:12 -0800

    Add proofreading skeleton

---
+++ src/scripts/app/activities/proofreadings/module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports =
+
+angular.module('quill-grammar.activities.sentences', [
+
+])
+.config(require('./config.js'))
+.controller('ProofreadingsCtrl', require('./controller.js'));
