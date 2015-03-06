104ec66104936860ee36ad8d3cbb0bd496c0f8cb
wlaurance <w.laurance@gmail.com>
2015-03-05 17:22:35 -0800

    Send a message through porthole

--- src/scripts/app/play/sentences/results.controller.js
+++ src/scripts/app/play/sentences/results.controller.js
@@ -1,8 +1,18 @@
+/*global Porthole*/
 'use strict';
 
 /*@ngInject*/
 module.exports = function($state) {
+  var windowProxy = new Porthole.WindowProxy();
+  var postObj = {
+    action: 'activity_complete',
+  };
+
   if ($state.params.student) {
+    postObj.id = $state.params.student;
   }
-  console.log(Porthole);
+
+  windowProxy.post(postObj);
+
+
 };
