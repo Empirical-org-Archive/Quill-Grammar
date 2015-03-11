0bed51948199f6fe8c883363b5f4628efa9b73f5
wlaurance <w.laurance@gmail.com>
2015-03-11 14:15:28 -0700

    Revert "Style the wrapper iframe" and rm iframe wrapper
    This reverts commit fb9b87f63c8e496969e6329dc2eea26a7f16fcc2.

--- src/scripts/app/play/proofreadings/config.js
+++ src/scripts/app/play/proofreadings/config.js
@@ -10,14 +10,9 @@ function configure ($stateProvider) {
     controller: 'ProofreadingPlayCtrl',
     url: '/play/pf?uid&brainpop'
   })
-  .state('play-partner-pf-iframe', {
-    parent: 'app',
-    template: '<iframe class="partner-iframe" src="/play/partner-pf-iframe" scrolling="auto"></iframe>',
-    url: '/play/partner-pf'
-  })
   .state('play-partner-pf-integration-start', {
     parent: 'app',
     templateUrl: 'proofreadings.partner.html',
-    url: '/play/partner-pf-iframe'
+    url: '/play/partner-pf'
   });
 };
