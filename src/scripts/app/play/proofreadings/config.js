fb9b87f63c8e496969e6329dc2eea26a7f16fcc2
wlaurance <w.laurance@gmail.com>
2015-03-11 13:33:19 -0700

    Style the wrapper iframe

--- src/scripts/app/play/proofreadings/config.js
+++ src/scripts/app/play/proofreadings/config.js
@@ -10,9 +10,14 @@ function configure ($stateProvider) {
     controller: 'ProofreadingPlayCtrl',
     url: '/play/pf?uid&brainpop'
   })
+  .state('play-partner-pf-iframe', {
+    parent: 'app',
+    template: '<iframe class="partner-iframe" src="/play/partner-pf-iframe" scrolling="auto"></iframe>',
+    url: '/play/partner-pf'
+  })
   .state('play-partner-pf-integration-start', {
     parent: 'app',
     templateUrl: 'proofreadings.partner.html',
-    url: '/play/partner-pf'
+    url: '/play/partner-pf-iframe'
   });
 };
