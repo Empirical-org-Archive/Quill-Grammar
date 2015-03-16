519d56072f3d1b7a92e5b8c4ba8353ea948ac28f
wlaurance <w.laurance@gmail.com>
2015-03-16 11:47:45 -0700

    Add concept overview directive

--- src/scripts/app/play/sentences/module.js
+++ src/scripts/app/play/sentences/module.js
@@ -6,5 +6,11 @@ angular.module('quill-grammar.play.sentences', [
   require('./../../../services/sentenceWriting.js').name,
 ])
 .config(require('./config.js'))
+.directive('sentenceWritingConceptOverview', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'sentences.concept-overview.html'
+  };
+})
 .controller('ResultsController', require('./results.controller.js'))
 .controller('SentencePlayCtrl', require('./controller.js'));
