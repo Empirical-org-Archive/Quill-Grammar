f376b5fb5e26d8a6716c3c2268214848861bc8f2
wlaurance <w.laurance@gmail.com>
2015-03-23 11:53:56 -0700

    Add results header

--- src/scripts/app/play/results/module.js
+++ src/scripts/app/play/results/module.js
@@ -4,4 +4,4 @@ module.exports =
 
 angular.module('quill-grammar.play.results', [])
 .config(require('./config.js'))
-.controller(require('./controller.js'));
+.controller('InternalResultsCtrl', require('./controller.js'));
