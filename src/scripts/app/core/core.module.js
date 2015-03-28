087a2a86ef47079b43fb2dc69524878d7b7b039c
wlaurance <w.laurance@gmail.com>
2015-03-27 18:28:52 -0700

    Auto focus the first word in the paragraph
    See #30

--- src/scripts/app/core/core.module.js
+++ src/scripts/app/core/core.module.js
@@ -1,10 +1,16 @@
 'use strict';
+
+/*
+ * Production Browerify
+ */
+require('ng-autofocus/autofocus');
 module.exports =
 
 angular
   .module('quill-grammar.core', [
     'ui.router',
     'LocalStorageModule',
+    'autofocus',
     require('../../../../.tmp/config').name,
     require('../../../../.tmp/templates').name,
     require('../../directives/index.js').name,
