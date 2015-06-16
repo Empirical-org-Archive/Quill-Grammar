e662c06bc8f507c8d02534fe6d72b563f810bfa2
wlaurance <w.laurance@gmail.com>
2015-06-16 15:09:54 -0700

    Pull the 3 highlighted PF activities into a two modules

---
+++ src/scripts/app/play/proofreadings/pfImages.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  '70B-T6vLMTM9zjQ9LCwoCg': 'the_princes_and_the_turtle_story_header.png',
+  'MJCtkml_69W2Dav79v4r9Q': 'ernest_shackleton_story_header.png',
+  'Yh49ICvX_YME8ui7cDoFXQ': 'the_apollo_8_photograph_story_header.png'
+};
--- src/scripts/app/play/results/controller.js
+++ src/scripts/app/play/results/controller.js
@@ -13,17 +13,9 @@ function InternalResultsController (
    * TODO move this out of here
    */
   //Add in some custom images for the 3 stories we are showcasing
-  $scope.pfImages = {
-    '70B-T6vLMTM9zjQ9LCwoCg': 'the_princes_and_the_turtle_story_header.png',
-    'MJCtkml_69W2Dav79v4r9Q': 'ernest_shackleton_story_header.png',
-    'Yh49ICvX_YME8ui7cDoFXQ': 'the_apollo_8_photograph_story_header.png'
-  };
+  $scope.pfImages = require('./../proofreadings/pfImages');
 
-  $scope.pfTitles = {
-    '70B-T6vLMTM9zjQ9LCwoCg': 'The Princess and the Turtle',
-    'MJCtkml_69W2Dav79v4r9Q': 'Ernest Shackleton Escapes the Antarctic',
-    'Yh49ICvX_YME8ui7cDoFXQ': 'The Apollo 8 Photograph'
-  };
+  $scope.pfTitles = require('./../proofreadings/pfTitles');
 
   if ($state.params.passageId) {
     $scope.passageImageUrl = $scope.pfImages[$state.params.passageId];
