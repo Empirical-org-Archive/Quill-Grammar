54c6d88453b4b14b08fcfd38475f7c771af69c71
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-07-08 17:03:25 -0500

    Fix linting errors

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,9 @@
+'use strict';
+
 // Karma configuration
 // Generated on Tue Jul 07 2015 20:44:56 GMT-0500 (CDT)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -25,49 +27,40 @@ module.exports = function(config) {
       'test/**/*.spec.js'
     ],
 
-
     // list of files to exclude
     exclude: [
       'build/*.js.map'
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
-}
+  });
+};
