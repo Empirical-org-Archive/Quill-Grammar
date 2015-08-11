992a8e21c8c1c889555d4ad1968fbeddda783ec6
wlaurance <w.laurance@gmail.com>
2015-08-11 12:42:44 -0700

    Add /cms
    With two links, activities and concepts
    
    See #126

--- src/scripts/app/cms/cms.config.js
+++ src/scripts/app/cms/cms.config.js
@@ -6,6 +6,16 @@ function configure ($stateProvider) {
     .state('cms', {
       parent: 'app',
       url: '/cms',
+      templateUrl: 'cms.base.html',
+      controller: 'cms',
+      abstract: true,
+      data: {
+        //authenticateUser: true
+      }
+    })
+    .state('cms-list', {
+      parent: 'app',
+      url: '/cms',
       templateUrl: 'cms.html',
       controller: 'cms',
       data: {
