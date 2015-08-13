245700f5ba9bc324c3e21157961785973a8ffb8f
wlaurance <w.laurance@gmail.com>
2015-08-13 14:29:36 -0700

    Add failing concepts test

--- src/scripts/app/cms/grammarActivities/directives/index.js
+++ src/scripts/app/cms/grammarActivities/directives/index.js
@@ -4,6 +4,7 @@ module.exports =
 angular.module('quill-grammar.cms.grammarActivities.directives', [
   'dynform',
   require('../../../../services/lms/theme.js').name,
+  require('../../../../services/lms/concept.js').name,
 ])
 .controller('GrammarActivityFormCtrl', require('./grammarActivityForm.controller.js'))
 .directive('grammarActivityForm', require('./grammarActivityForm.directive.js'))
