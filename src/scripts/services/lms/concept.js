0d7512a033ceec7411831e0a648023fc7c5ff749
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-08-31 16:34:05 -0400

    Fix ConceptService test

--- src/scripts/services/lms/concept.js
+++ src/scripts/services/lms/concept.js
@@ -1,40 +1,37 @@
 'use strict';
 
-var R = require('ramda');
-
 module.exports =
 angular.module('quill-grammar.services.lms.concept', [
-  'underscore'
+  'underscore',
+  require('../../../../.tmp/config').name,
 ])
 /*@ngInject*/
 .factory('ConceptService', function (empiricalBaseURL, $http) {
   var url = empiricalBaseURL + '/concepts';
 
   this.get = function () {
-    var accumulator = {concept_level_0: [],
-                       concept_level_1: [],
-                       concept_level_2: []};
-    var groupByLevel = R.reduce(function (acc, concept) {
-      switch (concept.level) {
-        case 0: {
-          acc.concept_level_0.push(concept);
-          break;
-        }
-        case 1: {
-          acc.concept_level_1.push(concept);
-          break;
-        }
-        case 2: {
-          acc.concept_level_2.push(concept);
-          break;
-        }
-      }
-      return acc;
-    }, accumulator);
-
     return $http.get(url).then(function (response) {
       var concepts = response.data.concepts;
-      return groupByLevel(concepts);
+      var accumulator = {concept_level_0: [],
+                         concept_level_1: [],
+                         concept_level_2: []};
+      return _.reduce(concepts, function (acc, concept) {
+        switch (concept.level) {
+          case 0: {
+            acc.concept_level_0.push(concept);
+            break;
+          }
+          case 1: {
+            acc.concept_level_1.push(concept);
+            break;
+          }
+          case 2: {
+            acc.concept_level_2.push(concept);
+            break;
+          }
+        }
+        return acc;
+      }, accumulator);
     });
   };
   return this;
--- test/services/lms/concept.spec.js
+++ test/services/lms/concept.spec.js
@@ -4,15 +4,31 @@
 describe('ConceptService', function () {
   beforeEach(module('quill-grammar.services.lms.concept'));
 
-  var conceptService;
+  var conceptService,
+      $httpBackend;
 
   beforeEach(function () {
-    inject(function (ConceptService) {
+    module(function ($provide) {
+      $provide.constant('empiricalBaseURL', 'http://foo.bar/api/v1');
+    });
+
+    inject(function (ConceptService, _$httpBackend_) {
       conceptService = ConceptService;
+      $httpBackend = _$httpBackend_;
     });
   });
 
-  describe('API Concepts', function () {
+  describe('#get', function () {
+    var fakeConceptsResponse = [
+      {name: 'foobar', uid: 'baz'},
+      {name: 'buzzbar', uid: 'quux'}
+    ];
+
+    beforeEach(function () {
+      $httpBackend.expectGET('http://foo.bar/api/v1/concepts')
+                  .respond(fakeConceptsResponse);
+    });
+
     it('gets all concepts', function (done) {
       conceptService.get().then(function (conceptLevels) {
         expect(conceptLevels).to.have.property('concept_level_0');
@@ -21,11 +37,12 @@ describe('ConceptService', function () {
         _.each(conceptLevels, function (l) {
           _.each(l, function (c) {
             expect(c.uid).to.be.a('string');
-            expect(c.title).to.be.a('string');
+            expect(c.name).to.be.a('string');
           });
         });
         done();
       });
+      $httpBackend.flush();
     });
   });
 });
