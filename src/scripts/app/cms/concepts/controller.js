a0afc8ab8f52821293b55d64a25aba7d7eab1a05
wlaurance <w.laurance@gmail.com>
2015-09-09 18:38:54 -0700

    Add specs for test
    count of concepts
    
    count of total questions

--- src/scripts/app/cms/concepts/controller.js
+++ src/scripts/app/cms/concepts/controller.js
@@ -8,9 +8,24 @@ function ConceptsCmsCtrl (
 ) {
   ConceptsFBService.get().then(function (c) {
     $scope.concepts = c;
+    console.log(c);
   });
 
   $scope.getQuestionLength = function (questions) {
     return _.keys(questions).length;
+  };
+
+  $scope.getTotalConcepts = function(concepts) {
+    return _.keys(concepts).length;
+  };
+
+  $scope.getTotalQuestions = function(concepts) {
+    return _.reduce(concepts, function(sum, c) {
+
+      if (_.isNaN(sum)) {
+        sum = 0;
+      }
+      return Number(sum) + Number(_.keys(c.questions).length);
+    });
   };
 };
