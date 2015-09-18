d9eb07fd960615886566bab2b34c737f503b303b
wlaurance <w.laurance@gmail.com>
2015-09-18 11:09:59 -0700

    Key grammar Activity concepts on fb id
    See #148

--- src/scripts/app/cms/grammarActivities/create.controller.js
+++ src/scripts/app/cms/grammarActivities/create.controller.js
@@ -4,12 +4,33 @@ module.exports =
 
 /*@ngInject*/
 function GrammarActivitiesCreateCmsCtrl (
-  $scope
+  $scope, _
 ) {
   $scope.grammarActivity = {};
   $scope.grammarActivity.question_set = [{}];
 
+  function buildConcepts(set) {
+    return _.chain(set)
+      .map(function (s) {
+        return [s.concept_level_0.$id, {
+          quantity: s.number_of_questions,
+          ruleNumber: s.concept_level_0.ruleNumber
+        }];
+      })
+      .object()
+      .value();
+  }
+
   $scope.processGrammarActivityForm = function () {
     console.log('Processing ', $scope.grammarActivity);
+    var ga = $scope.grammarActivity;
+    var newGrammarActivity = {
+      title: ga.title,
+      description: ga.description,
+      concepts: buildConcepts(ga.question_set)
+    };
+
+    console.log(newGrammarActivity);
+
   };
 };
