8c45bf83fe32a5e02c4732f643c5ea8c61e243fb
wlaurance <w.laurance@gmail.com>
2015-09-25 17:45:09 -0700

    Add topic Categories directive

--- src/scripts/app/cms/grammarActivities/directives/index.js
+++ src/scripts/app/cms/grammarActivities/directives/index.js
@@ -7,6 +7,7 @@ angular.module('quill-grammar.cms.grammarActivities.directives', [
   require('../../../../services/lms/topicCategory.js').name,
   require('../../base_directives/concepts/index.js').name,
   require('../../base_directives/standards/index.js').name,
+  require('../../base_directives/topicCategories/index.js').name,
 ])
 .controller('GrammarActivityFormCtrl', require('./grammarActivityForm.controller.js'))
 .directive('grammarActivityForm', require('./grammarActivityForm.directive.js'))
