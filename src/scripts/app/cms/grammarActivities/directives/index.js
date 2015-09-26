1ebdde68bb6efea70fa48f8e0e16a742c2733839
wlaurance <w.laurance@gmail.com>
2015-09-26 10:25:46 -0700

    rm dynforms import

--- src/scripts/app/cms/grammarActivities/directives/index.js
+++ src/scripts/app/cms/grammarActivities/directives/index.js
@@ -3,7 +3,6 @@
 /*@ngInject*/
 module.exports =
 angular.module('quill-grammar.cms.grammarActivities.directives', [
-  'dynform',
   require('../../../../services/lms/topicCategory.js').name,
   require('../../base_directives/concepts/index.js').name,
   require('../../base_directives/standards/index.js').name,
