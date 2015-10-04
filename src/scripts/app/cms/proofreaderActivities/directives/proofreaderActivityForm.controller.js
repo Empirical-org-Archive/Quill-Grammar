ab75367815b4cd032c5dadc6f97a56e634f1e269
wlaurance <w.laurance@gmail.com>
2015-10-03 17:32:25 -0700

    rm unused code

--- src/scripts/app/cms/proofreaderActivities/directives/proofreaderActivityForm.controller.js
+++ src/scripts/app/cms/proofreaderActivities/directives/proofreaderActivityForm.controller.js
@@ -11,22 +11,5 @@ module.exports =
 
 /*@ngInject*/
 function ProofreaderActivityFormCtrl (
-  $scope, _, TopicCategoryService
 ) {
-  $scope.concepts = {};
-
-  TopicCategoryService.get().then(function (topicCategories) {
-    $scope.topicCategories = topicCategories;
-  });
-
-  $scope.removeQuestionFromSet = function (qs) {
-    if ($scope.proofreaderActivity.concepts) {
-      $scope.proofreaderActivity.concepts = _.without(
-        $scope.proofreaderActivity.concepts, qs
-      );
-    }
-  };
-  $scope.addAnotherQuestionToSet = function () {
-    $scope.proofreaderActivity.concepts.push({});
-  };
 };
