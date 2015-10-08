f128bf78a6c2864908ce77b7e8e4e30f9526039f
Donald McKendrick <ddmckendrick@gmail.com>
2015-10-08 00:00:28 -0400

    Fix more listing issues

--- src/scripts/directives/grammarElements/ruleQuestion/ngEnter.js
+++ src/scripts/directives/grammarElements/ruleQuestion/ngEnter.js
@@ -2,8 +2,8 @@
 module.exports = function () {
   return function (scope, element, attrs) {
     element.bind('keydown keypress', function (event) {
-      if(event.which === 13) {
-        scope.$apply(function (){
+      if (event.which === 13) {
+        scope.$apply(function () {
           scope.$eval(attrs.ngEnter);
         });
 
