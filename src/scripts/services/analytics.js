88089f0f2a6a45d334a180368e09b9e066f89ba8
Donald McKendrick <ddmckendrick@gmail.com>
2015-10-28 23:21:09 -0400

    tidy indentation

--- src/scripts/services/analytics.js
+++ src/scripts/services/analytics.js
@@ -35,32 +35,34 @@ angular.module('quill-grammar.services.analytics', [
   return {
     trackSentenceWritingSubmission: trackSentenceWritingSubmission
   };
-}).factory('errorHttpInterceptor', ['$q', '$window', function ($q, $window) {
-        return {
-            responseError: function responseError(rejection) {
-                if ($window.atatus) {
-                    var message = 'AJAX Error: ' +
-                                   rejection.config.method + ' ' +
-                                   rejection.status + ' ' +
-                                   rejection.config.url;
-                    atatus.notify(new Error(message), {
-                        config: rejection.config,
-                        status: rejection.status
-                    });
-                }
-                return $q.reject(rejection);
-            }
-        };
-    }])
-    .config(['$httpProvider', function($httpProvider) {
-        $httpProvider.interceptors.push('errorHttpInterceptor');
-    }]).factory('$exceptionHandler', ['$window', function ($window) {
-    return function (exception, cause) {
-        if (exception.stack) {
-            exception.stack = exception.stack.replace('new <anonymous>', '<anonymous>');
-        }
-        if ($window.atatus) {
-            $window.atatus.notify(exception);
-        }
-    };
+})
+.factory('errorHttpInterceptor', ['$q', '$window', function ($q, $window) {
+  return {
+    responseError: function responseError(rejection) {
+      if ($window.atatus) {
+        var message = 'AJAX Error: ' +
+          rejection.config.method + ' ' +
+          rejection.status + ' ' +
+          rejection.config.url;
+        atatus.notify(new Error(message), {
+          config: rejection.config,
+          status: rejection.status
+        });
+      }
+      return $q.reject(rejection);
+    }
+  };
+}])
+.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('errorHttpInterceptor');
+}])
+.factory('$exceptionHandler', ['$window', function ($window) {
+  return function (exception, cause) {
+      if (exception.stack) {
+          exception.stack = exception.stack.replace('new <anonymous>', '<anonymous>');
+      }
+      if ($window.atatus) {
+          $window.atatus.notify(exception);
+      }
+  };
 }]);
