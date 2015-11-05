3d5bc3df3acf275aede764c4fea8d603ac4eb852
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-05 16:01:04 -0500

    Passes linting

--- src/scripts/services/analytics.js
+++ src/scripts/services/analytics.js
@@ -62,38 +62,38 @@ angular.module('quill-grammar.services.analytics', [
   $httpProvider.interceptors.push('errorHttpInterceptor');
 }])
 
-.factory("errors", ['$window', '$log', function errorLoggerFactory( $window, $log ) {
-  // Return the public API.
-  return({
-    report: report
-  });
+.factory('errors', ['$window', '$log', function errorLoggerFactory ($window, $log) {
   // I report errors to the remote server.
-  function report( error ) {
+  function report(error) {
     // In this case, we're going to be using New Relic's Browser API to
     // report JavaScript errors that originate from within the AngularJS
     // application try / catch blocks (which is why the global error
     // handler doesn't see them). But, since New Relic is a per-server
     // cost, we might not have it enabled in every environment. Let's
     // check to see if the API exists before we try to use it.
-    if ( $window.NREUM && $window.NREUM.noticeError ) {
+    if ($window.NREUM && $window.NREUM.noticeError) {
       try {
-        $window.NREUM.noticeError( error );
+        $window.NREUM.noticeError(error);
       // If logging errors is causing an error, just swallow those
       // errors; attempting to log these errors might lock the browser
       // in an infinite loop.
-      } catch ( newRelicError ) {
-        $log.error( newRelicError );
+      } catch (newRelicError) {
+        $log.error(newRelicError);
       }
     } else {
-      $log.info( "New Relic not available to record error." );
+      $log.info('New Relic not available to record error.');
     }
     if ($window.atatus) {
       $window.atatus.notify(error);
     } else {
-      $log.info( "Atatus not available to record error." );
+      $log.info('Atatus not available to record error.');
     }
     $log.error(error);
   }
+  // Return the public API.
+  return ({
+    report: report
+  });
 }])
 
 .factory('$exceptionHandler', ['$window', 'errors', function ($window, errors) {
