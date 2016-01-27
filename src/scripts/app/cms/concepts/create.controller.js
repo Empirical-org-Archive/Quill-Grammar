c040a3b862d59a46c7e56f5f0665513a88f15b96
Donald McKendrick <ddmckendrick@gmail.com>
2016-01-27 14:25:16 +0000

    allow for concept level one activities to be created

--- src/scripts/app/cms/concepts/create.controller.js
+++ src/scripts/app/cms/concepts/create.controller.js
@@ -4,16 +4,24 @@ module.exports =
 
 /*@ngInject*/
 function ConceptsCreateCmsCtrl (
-  $scope, ConceptsFBService, $state
+  $scope, ConceptsFBService, ConceptService, $state
 ) {
   $scope.concept = {};
 
   $scope.processConceptForm = function () {
-    ConceptsFBService.add($scope.concept).then(function () {
-      $state.go('cms-concepts');
-      return;
-    }, function (error) {
-      console.error(error);
+    ConceptService.post({concept: {name: $scope.concept.name, parent_uid: $scope.concept.concept_level_1.uid}}).then(function (res) {
+      $scope.concept.concept_level_0 = {name: res.data.concept.name,
+                                        id: res.data.concept.id,
+                                        uid: res.data.concept.uid,
+                                        parent_id: res.data.concept.parent_id,
+                                        level: 0
+                                        };
+      ConceptsFBService.add($scope.concept).then(function () {
+        $state.go('cms-concepts');
+        return;
+      }, function (error) {
+        console.error(error);
+      });
     });
   };
 };
